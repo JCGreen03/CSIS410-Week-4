348db9958630d27a53a13a648cac9f3cb6398301
JCGreen03 <160683432+JCGreen03@users.noreply.github.com>
2025-04-17 11:37:27 -0400

    Delete Sessions directory

--- Sessions/Components/EmployeeVincent.php
+++
@@ -1,9 +0,0 @@
-<?php
-    echo"<div>";
-    echo "<img class=employeebio src='./Media/Vincent.jpg' alt='Vincent'>";
-    echo "<p>Name: $vincent->employee_name</p>";
-    echo "<p>Rank: $vincent->employee_rank</p>";
-    echo "<p>Email: $vincent->employee_email</p>";
-    echo "<p>Favorite Book: $vincent->employee_favoriteBook</p>";
-    echo "</div>";
-?>
